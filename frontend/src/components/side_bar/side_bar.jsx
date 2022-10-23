09603705b3835c23243f0cdeaa33a67a280d1071
Deanding <dingtian.ding@gmail.com>
2022-10-23 18:05:59 -0400

    heroku push

--- frontend/src/components/side_bar/side_bar.jsx
+++ frontend/src/components/side_bar/side_bar.jsx
@@ -17,17 +17,17 @@ const drawerWidth = 240;
 
 export default function SideBar() {
   const states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
-
+  
+  // const handleClick = (id, e) => {
+  //   e.preventDefault();
+  //   if (history.location.pathname === `/USAStates/${id}`) return null;
+  //       history.push({
+  //         pathname: `/USAStates/${id}`,
+  //       });
+  // };
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      {/* <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Clipped drawer
-          </Typography>
-        </Toolbar>
-      </AppBar> */}
       <Drawer
         variant="permanent"
         sx={{
@@ -36,24 +36,17 @@ export default function SideBar() {
           [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
         }}
       >
-        {/* <Toolbar /> */}
         <Box className='sidebar' sx={{ overflow: 'auto' }}>
           <List>
-            {['Create Post'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <ListItemIcon /> : <ListItemIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
             {states.map((text, index) => (
-              <ListItem key={text} disablePadding>
+              <ListItem key={text} disablePadding 
+              // onClick={(e) =>
+              //   handleClick(
+              //     item.id,
+              //     e
+              //   )
+              // }
+              >
                 <ListItemButton>
                   <ListItemIcon>
                     {index % 2 === 0 ? <ListItemIcon /> : <ListItemIcon />}
