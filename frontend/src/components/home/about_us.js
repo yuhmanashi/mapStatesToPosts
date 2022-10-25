ad505c275925bd232ba99c7d6d26ea61865c40aa
yuhmanashi <yzxu28@gmail.com>
2022-10-24 20:24:25 -0700

    added profile pictures

--- frontend/src/components/home/about_us.js
+++ frontend/src/components/home/about_us.js
@@ -1,50 +1,119 @@
 import React from 'react';
-import { FaGithub, FaRegUserCircle, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaRegUserCircle, FaLinkedin, FaAngellist } from 'react-icons/fa';
 import './about_us.css'
 
 export default class AboutUs extends React.Component {
     render(){
         return (
             <div className="about-us-container">
-                <h1>About Us</h1>
-                <hr/>
                 <div className="about-us">
                     <ul className='yao-xu'>
-                        <h4>Yao Xu:</h4>
-                        <li>
-                            <a href="https://github.com/yuhmanashi" target="_blank">
-                                <FaGithub />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.linkedin.com/in/yaoxu03/" target="_blank">
-                                <FaLinkedin />
-                            </a>
-                        </li>
+                        <div className='selfie'>
+                            <img src={require('./pics/yao.png')}></img>
+                        </div>
+                        <h4>Yao Xu</h4>
+                        <ul className='links'>
+                            <li>
+                                <a href="https://github.com/yuhmanashi" target="_blank">
+                                    <FaGithub />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/yaoxu03/" target="_blank">
+                                    <FaLinkedin />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" target="_blank">
+                                    <FaAngellist />
+                                </a>
+                            </li>
+                        </ul>
                     </ul>
-
+                    
                     <ul className='dean-ding'>
-                        <h4>Dean Ding:</h4>
-                        <li>
-                            <a href="https://github.com/dingtianding" target="_blank">
-                                <FaGithub />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.linkedin.com/in/deanding/" target="_blank">
-                                <FaLinkedin />
-                            </a>
-                        </li>
+                        <div className='selfie'>
+                            <img src={require('./pics/dd.png')}></img>
+                        </div>
+                        <h4>Dean Ding</h4>
+                        <ul className='links'>
+                            <li>
+                                <a href="https://github.com/dingtianding" target="_blank">
+                                    <FaGithub />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/deanding/" target="_blank">
+                                    <FaLinkedin />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" target="_blank">
+                                    <FaAngellist />
+                                </a>
+                             </li>
+                        </ul>
+                        
                     </ul>
 
-                    <ul className="project-link">
+                    <ul className='kayi-leung'>
+                        <div className='selfie'>
+                            <img src={require('./pics/ky.png')}></img>
+                        </div>
+                        <h4>Kayi Leung</h4>
+                        
+                        <ul className='links'>
+                            <li>
+                                <a href="https://github.com/yuhmanashi" target="_blank">
+                                    <FaGithub />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/yaoxu03/" target="_blank">
+                                    <FaLinkedin />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" target="_blank">
+                                    <FaAngellist />
+                                </a>
+                            </li>
+                        </ul>
+                    </ul>
+
+                    <ul className='sean-mackey'>
+                        <div className='selfie'>
+                            <img src={require('./pics/smackey.png')}></img>
+                        </div>
+                        <h4>Sean Mackey</h4>
+                        
+                        <ul className='links'>
+                            <li>
+                                <a href="https://github.com/yuhmanashi" target="_blank">
+                                    <FaGithub />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/yaoxu03/" target="_blank">
+                                    <FaLinkedin />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" target="_blank">
+                                    <FaAngellist />
+                                </a>
+                            </li>
+                        </ul>
+                    </ul>
+
+                    {/* <ul className="project-link">
                         <h4>Project Repo:</h4>
                         <li>
                             <a href="https://github.com/KayiLeung/mapStatesToPost" target="_blank">
                                 <FaGithub />
                             </a>
                         </li>
-                    </ul>
+                    </ul> */}
                 </div>
             </div>
         )
--- frontend/src/components/session/signup_form_container.js
+++ frontend/src/components/session/signup_form_container.js
@@ -3,7 +3,7 @@ import { signup, login, removeErrors } from '../../actions/session_actions';
 import { closeModal, openModal } from '../../actions/modal_actions';
 import SignupForm from './signup_form';
 
-const mapStateToProps = (state, props) => {
+const mapStateToProps = (state) => {
   return {
     signedIn: state.session.isSignedIn,
     errors: state.errors.session,
