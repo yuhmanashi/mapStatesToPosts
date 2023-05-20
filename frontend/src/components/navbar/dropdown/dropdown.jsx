10c545397d7cea2b1146e3d59a0d661fd2b04115
yuhmanashi <yzxu28@gmail.com>
2023-05-19 19:41:48 -0700

    dropdown more or less finished

--- frontend/src/components/navbar/dropdown/dropdown.jsx
+++ frontend/src/components/navbar/dropdown/dropdown.jsx
@@ -1,81 +1,56 @@
 import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Link } from 'react-router-dom';
 
-const options = [
-  'Show some love to MUI',
-  'Show all notification content',
-  'Hide sensitive notification content',
-  'Hide all notification content',
-];
+export default function PositionedMenu(props) {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+    
+    const logout = () => {
+        props.props.logout();
+        handleClose();
+    }
+    
+    const aboutUsModal = () => {
+        props.props.openModal('about-us');
+        handleClose();
+    }
 
-export default function SimpleListMenu(props) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
-  const open = Boolean(anchorEl);
-  const handleClickListItem = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuItemClick = (event, index) => {
-    setSelectedIndex(index);
-    setAnchorEl(null);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const logout = () => {
-    props.props.logout();
-    handleClose();
-  }
-
-  const aboutUsModal = () => {
-    props.props.openModal('about-us');
-    handleClose();
-  }
-
-  return (
-    <div>
-      <List
-        component="nav"
-        aria-label="Device settings"
-        sx={{ bgcolor: 'background.paper' }}
-      >
-        <ListItem
-          button
-          id="lock-button"
-          aria-haspopup="listbox"
-          aria-controls="lock-menu"
-          aria-label="when device is locked"
+    return (
+      <div>
+        <Button
+          id="demo-positioned-button"
+          aria-controls={open ? 'demo-positioned-menu' : undefined}
+          aria-haspopup="true"
           aria-expanded={open ? 'true' : undefined}
-          onClick={handleClickListItem}
+          onClick={handleClick}
         >
           <AccountCircleIcon />
-        </ListItem>
-      </List>
-      <Menu
-        id="lock-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'lock-button',
-          role: 'listbox',
-        }}
-      >
-        <MenuItem>
-            <Link to={`/users/${props.props.userId}`}>Profile</Link>
-        </MenuItem>
-        <MenuItem onClick={aboutUsModal}>About Us</MenuItem>
-        <MenuItem onClick={logout}>Logout</MenuItem>
-      </Menu>
-    </div>
-  );
+        </Button>
+        <Menu
+          id="demo-positioned-menu"
+          aria-labelledby="demo-positioned-button"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+        >
+            <MenuItem>
+                <Link to={`/users/${props.props.userId}`}>Profile</Link>
+            </MenuItem>
+            <MenuItem onClick={aboutUsModal}>About Us</MenuItem>
+            <MenuItem onClick={logout}>Logout</MenuItem>
+        </Menu>
+      </div>
+    );
 }
